be1527d82740c8f6074772a70aa92eece7d5b818
Clayton Ives <102875676+Duke-of-Disastr@users.noreply.github.com>
2023-02-22 20:22:44 -0500

    Update InputBuffer.cs
    Added Content

--- InputBuffer.cs
+++ InputBuffer.cs
@@ -1,6 +1,64 @@
+using System.Collections;
 using UnityEngine;
 
-public class InputBuffer 
+public class InputBuffer
 {
+    public delegate void InputBufferEvent();
+    public float bufferTime;
 
+    private bool bufferStarted; // flag to indicate if the buffer has started
+    private float timer; // timer for buffer time
+  
+    private InputBufferEvent bufferEvent; // function to be executed once buffer is complete
+    private System.Func<bool>[] requirements;
+
+    
+    
+    public InputBuffer(InputBufferEvent eventToExecute, float bufferDuration, params System.Func<bool>[] _requirements)
+    {
+        bufferEvent = eventToExecute;
+        requirements = _requirements;
+        bufferTime = bufferDuration;
+        bufferStarted = false;
+        timer = 0f;
+    }
+    
+    public StartBuffer() 
+    {
+        bufferStarted = true;
+        timer = 0f;
+        StartCoroutine(CheckBufferRequirements(requirements));
+    }
+
+    // Coroutine to check the buffer requirements
+    private IEnumerator CheckBufferRequirements(params System.Func<bool>[] requirements)
+    {
+        while (timer < bufferTime)
+        {
+            timer += Time.deltaTime;
+
+            // Check if all requirements are met
+            bool requirementsMet = true;
+            foreach (System.Func<bool> requirement in requirements)
+            {
+                if (!requirement())
+                {
+                    requirementsMet = false;
+                    break;
+                }
+            }
+
+            // If all requirements are met, execute the buffer event
+            if (requirementsMet)
+            {
+                bufferEvent();
+                break;
+            }
+
+            yield return null;
+        }
+
+        bufferStarted = false;
+    }
 }
+
